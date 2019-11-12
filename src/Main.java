5a879c0d8ea0b0c6616a3cfa3b0c5ce8af3226b8
Kempezz <57679787+Kempezz@users.noreply.github.com>
2019-11-12 19:55:50 +0100

    Update and rename Main.java to Main1.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-// sout -> System.out.println();
-// CTRL + D -> duplikowanie lini kodu
-public class Main {
-    {public static void main(String[] args)
-        System.out.println("Hello World!");
-    System.out.println("Publikujemy pierwszy program!!!");
-
-    }
-}
