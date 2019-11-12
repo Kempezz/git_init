12d44f82fe39887eda06c7ca063c154aad8de841
Kempezz <pawelkemp@gmail.com>
2019-11-12 19:41:34 +0100

    pierwsza zmiana w projekcie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+// sout -> System.out.println();
+// CTRL + D -> duplikowanie lini kodu
 public class Main {
-    public static void main(String[] args) {
+    {public static void main(String[] args)
+        System.out.println("Hello World!");
+    System.out.println("Publikujemy pierwszy program!!!");
 
     }
 }
